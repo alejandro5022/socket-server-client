9f894bb0018149045c9b12166acf1c23967e1b71
Alejandro <tarache.developer@gmail.com>
2020-10-17 18:12:24 -0300

    first commit

---
+++ client.py
@@ -0,0 +1,28 @@
+import socket
+
+HEADER = 64
+PORT = 1233
+FORMAT = "UTF-8"
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = '127.0.1.1'
+ADDR = (SERVER,PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' *(HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+send("hello! world")
+send("hello! Everyone")
+send("hello! Ale")
+texto = input(">>>")
+send(texto)
+
+send(DISCONNECT_MESSAGE)
